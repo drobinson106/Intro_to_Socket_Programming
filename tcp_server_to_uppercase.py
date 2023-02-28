b01ce0e3b5b4c0e716d2a3832a4e23cee5bb58ef
Donte Robinson <dontekendallrobinson@gmail.com>
2023-02-28 14:00:04 -0500

    Update tcp_server_to_uppercase.py

--- tcp_server_to_uppercase.py
+++ tcp_server_to_uppercase.py
@@ -1,11 +1,12 @@
 import socket
 
+#AF_INET is the internet family for IPv4 and SOCK_STREAM is the socket type for TCP
 HOST = "127.0.0.1" #localhost (IP address for a loopback interface)
 PORT = 65432
 
-#AF_INET is the internet family for IPv4 and SOCK_STREAM is the socket type for TCP
-#.accepts waits for a connection "conn" then creates a new socket object for "conn"
 #"data" is recieved from the client and .sendall(data.upper()) sends it back in all uppercase
+#.accepts waits for a connection "conn" then creates a new socket object for "conn"
+#Can only accept one response from the client before conection is closed
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
 	s.bind((HOST,PORT)) 
 	s.listen() 
