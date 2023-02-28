01fb448bbb88635832a1eb87a25d33970687ff3a
Donte Robinson <dontekendallrobinson@gmail.com>
2023-02-28 13:58:28 -0500

    Update tcp_server_reverse.py

--- tcp_server_reverse.py
+++ tcp_server_reverse.py
@@ -1,11 +1,11 @@
 import socket
 
+#AF_INET is the internet family for IPv4 and SOCK_STREAM is the socket type for TCP
 HOST = "127.0.0.1" #localhost (IP address for a loopback interface)
 PORT = 65432
 
-#AF_INET is the internet family for IPv4 and SOCK_STREAM is the socket type for TCP
+#"data" is recieved from the client and .sendall(data[::-1]) sends it back in reverse
 #.accepts waits for a connection "conn" then creates a new socket object for "conn"
-#"data" is recieved from the client and .sendall(data.upper()) sends it back in reverse
 #While loop is used to keep the connection open till it is closed by the client
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.bind((HOST,PORT)) 
