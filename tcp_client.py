881dd07afa85c11a9ebb571b2e0bb3331bda6d2d
Donte Robinson <dontekendallrobinson@gmail.com>
2023-02-28 14:01:54 -0500

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -3,7 +3,7 @@ import socket
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-#Creates a socket "s" that connects to the server then sends the message "Hello, world"
+#Creates a socket "s" that connects to the server then sends the message "Hello, world" to the server
 #"data" recieves the message from the server and prints it 
 #The message is set and recieved as bytes so .decode() is used to convert bytes(b) to string(str)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
